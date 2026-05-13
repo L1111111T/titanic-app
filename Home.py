f722f5ccd0bcc1fb6dd78ac64d744fc539222a73
Yitian Li <liyitian@udel.edu>
2026-05-13 18:58:51 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,16 +2,8 @@ import streamlit as st
 
 st.set_page_config(page_title="Home")
 
-st.title("Welcome to the Titanic App")
-
-st.write(
-    """
-    This Streamlit application analyzes the Titanic dataset
-    and predicts passenger survival probability.
-    """
-)
-
-st.info("Please open app.py to explore the analysis and prediction pages.")
+st.title("Welcome to the Home Page!")
+st.write("This is the home page for the Titanic App.")
 
 if st.button("Go to Titanic App"):
-    st.switch_page("app.py")
+    st.switch_page("pages/app.py")
